9e89c1125d3443b6124d809c4af717ecd1dcc1f3
Wellington Roth <wellington_roth@hotmail.com>
2020-12-07 11:53:27 -0300

    componente lista de tarefas 2

--- projeto-02-tarefas/tarefas/src/components/TaskGrid.vue
+++ projeto-02-tarefas/tarefas/src/components/TaskGrid.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="task-grid">
-    <template>
+    <template v-if="tasks.length">
       <Task 
         v-for="task in tasks" 
         :key="task.name"
         :task="task" 
       />
     </template>
+    <p v-else class="no-task">Sua vida está em dia :)</p>
   </div>
 </template>
 
@@ -24,5 +25,18 @@ import Task from "./Task.vue"
 </script>
 
 <style scoped>
+  .task-grid {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
 
+  .task-grid .task {
+    margin: 10px;
+  }
+
+  .no-task {
+    color: #AAA;
+    font-size: 1.7rem;
+  }
 </style>
