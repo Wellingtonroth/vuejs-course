0197d02d9cafde8f247d4f0ef76ab51490bcccf3
Wellington Roth <wellington_roth@hotmail.com>
2020-12-07 11:10:42 -0300

    componente tarefa

--- projeto-02-tarefas/tarefas/src/App.vue
+++ projeto-02-tarefas/tarefas/src/App.vue
@@ -1,11 +1,25 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<TaskGrid :tasks="tasks" />
 	</div>
 </template>
 
 <script>
+import TaskGrid from "./components/TaskGrid.vue"
+
 export default {
+	components: {
+		TaskGrid
+	},
+	data() { 
+		return {
+			tasks: [
+				{ name: 'Lavar a louça', peding: false },
+				{ name: 'Comprar blusa', peding: true },
+			]
+		}
+	}
 
 }
 </script>
