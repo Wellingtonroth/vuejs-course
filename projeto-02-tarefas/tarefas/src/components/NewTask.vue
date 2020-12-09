139341233401cd93ff03ad81e226379f8840e44d
Wellington Roth <wellington_roth@hotmail.com>
2020-12-09 15:21:07 -0300

    projeto 02 concluido

--- projeto-02-tarefas/tarefas/src/components/NewTask.vue
+++ projeto-02-tarefas/tarefas/src/components/NewTask.vue
@@ -1,56 +1,51 @@
 <template>
-  <div class="new-task">
-    <input 
-      v-model="name" 
-      @keydown.enter="add" 
-      type="text"
-      class="form-element"
-      placegolder="Nova tarefa?"
-    >
-    <button @click="add" class="form-element">+</button>
-  </div>
+    <div class="new-task">
+        <input v-model="name" @keydown.enter="add" type="text"
+            class="form-element" placeholder="Nova tarefa?">
+        <button class="form-element" @click="add">+</button>
+    </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return { 
-      name: ''
+    data() {
+        return {
+            name: ''
+        }
+    },
+    methods: {
+        add() {
+            this.$emit('taskAdded', { name: this.name })
+            this.name = ''
+        }
     }
-  },
-  methods: {
-    add() {
-      this.$emit('taskAdded', { name: this.name })
-      this.name = ''
-    }
-  }
 }
 </script>
 
 <style>
-  .new-task {
-    margin: 35px;
-  }
+    .new-task {
+        margin: 35px;
+    }
 
-  .form-element {
-    outline: none;
-    font-size: 2rem;
-    border: 1px solid #fff;
-    padding: 5px 10px 8px;
-    color: #fff;
-  }
+    .form-element {
+        outline: none;
+        font-size: 2rem;
+        border: 1px solid #FFF;
+        padding: 5px 10px 8px;
+        color: #FFF;
+    }
 
-  input.form-element {
-    width: 500px;
-    background: #FFF2;
-    border-top-left-radius: 8px;
-    border-bottom-left-radius: 8px;
-  }
+    input.form-element {
+        width: 500px;
+        background: #FFF2;
+        border-top-left-radius: 8px;
+        border-bottom-left-radius: 8px;
+    }
 
-  button.form-element {
-    border-left: none;
-    background-color: #2196f3;
-    border-top-right-radius: 8px;
-    border-bottom-right-radius: 8px;
-  }
+    button.form-element {
+        border-left: none;
+        background-color: #2196F3;
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+    }
 </style>
