7134f3d7e79f9fcf0d8838a2cfd4f3733dab860f
Wellington Roth <wellington_roth@hotmail.com>
2020-12-09 10:58:45 -0300

    componente nova tarefa 2

--- projeto-02-tarefas/tarefas/src/components/NewTask.vue
+++ projeto-02-tarefas/tarefas/src/components/NewTask.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="new-task">
-    <input v-model="name" @keydown.enter="add" type="text">
-    <button @click="add">+</button>
+    <input 
+      v-model="name" 
+      @keydown.enter="add" 
+      type="text"
+      class="form-element"
+      placegolder="Nova tarefa?"
+    >
+    <button @click="add" class="form-element">+</button>
   </div>
 </template>
 
@@ -22,5 +28,29 @@ export default {
 </script>
 
 <style>
+  .new-task {
+    margin: 35px;
+  }
 
+  .form-element {
+    outline: none;
+    font-size: 2rem;
+    border: 1px solid #fff;
+    padding: 5px 10px 8px;
+    color: #fff;
+  }
+
+  input.form-element {
+    width: 500px;
+    background: #FFF2;
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+  }
+
+  button.form-element {
+    border-left: none;
+    background-color: #2196f3;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+  }
 </style>
