8db607b07b8de8876f1091cdf265b44f5a387e3b
Wellington Roth <wellington_roth@hotmail.com>
2020-12-09 10:45:24 -0300

    componente nova tarefa

--- projeto-02-tarefas/tarefas/src/App.vue
+++ projeto-02-tarefas/tarefas/src/App.vue
@@ -1,16 +1,19 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<NewTask @taskAdded="addTask" />
 		<TaskGrid :tasks="tasks" />
 	</div>
 </template>
 
 <script>
 import TaskGrid from "./components/TaskGrid.vue"
+import NewTask from "./components/NewTask"
 
 export default {
 	components: {
-		TaskGrid
+		TaskGrid,
+		NewTask
 	},
 	data() { 
 		return {
@@ -19,9 +22,20 @@ export default {
 				{ name: 'Comprar blusa', pending: true },
 			]
 		}
-	}
+	},
+	methods: {
+		addTask(task) {
+			const sameName = t => t.name === task.name
+			const reallyNew = this.tasks.filter(sameName).length == 0
 
+			reallyNew && this.tasks.push({
+				name: task.name,
+				pending: task.pending || true
+			})
+		}
+	}
 }
+
 </script>
 
 <style>
